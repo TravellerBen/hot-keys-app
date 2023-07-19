c142d45bedba9b29eafa28f857b1a8b4d50dc958
TravellerBen <ben@the-laycocks.co.uk>
2023-07-19 19:32:46 +0100

    added keyboard and key styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img
-          src={logo}
-          className='App-logo'
-          alt='logo'
-        />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React!
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className='keyboard'>
+        <div className='key'>Q</div>
+        <div className='key'>W</div>
+        <div className='key'>E</div>
+        <div className='key'>R</div>
+        <div className='key'>T</div>
+        <div className='key'>Y</div>
+      </div>
+    </main>
   );
 }
 
